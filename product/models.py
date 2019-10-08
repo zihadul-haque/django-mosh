13c9ae7181196130855ee60e39e40a4358badaca
zihadul-haque <53473267+zihadul-haque@users.noreply.github.com>
2019-10-09 04:52:30 +0600

    initialized our project

---
+++ product/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name=models.CharField(max_length=255)
+    price=models.FloatField()
+    stock=models.IntegerField()
+    image_urls=models.CharField(max_length=2083)
+
+
+class Offer(models.Model):
+    code=models.CharField(max_length=10)
+    description=models.CharField(max_length=255)
+    discount=models.FloatField()
+
+
+
